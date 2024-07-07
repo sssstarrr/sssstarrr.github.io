3ce464b0cb9c2d4f84d59bca068867c1b2a5f02c
三水番 <1164442816@qq.com>
2024-07-07 19:09:10 +0800

    Site updated: 2024-07-07 19:09:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/hello-world/"];function toRandomPost(){
+var posts=["2024/07/07/123/","2024/07/07/每天一个linux命令/","2024/07/07/这是一篇新的博文/","2024/07/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
