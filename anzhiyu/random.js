665191a38b2f60f462c130a20e2c8daca5cf546d
三水番 <1164442816@qq.com>
2024-07-09 10:27:45 +0800

    Site updated: 2024-07-09 10:27:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/每天一个linux命令/","2024/07/07/迁移到hexo的第一篇博文/","2024/07/04/createvue2project/","2024/07/02/网站美化/","2024/06/29/排序算法总结/"];function toRandomPost(){
+var posts=["2024/07/07/迁移到hexo的第一篇博文/","2024/07/04/createvue2project/","2024/06/29/排序算法总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
